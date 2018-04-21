dda26b9c719d98fc1c7128c73c0d25934886d7ab
Yangchen Shen <shyangch@gmail.com>
2018-04-21 16:33:40 -0700

    Style Heaer with View

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,31 @@
 // import libraries for making a registerComponent
 import React from 'react';
-import { Text } from 'react-native'; //desctructure import
+import { Text, View } from 'react-native'; //desctructure import
 
 // make a component
 const Header = () => {
-  const { textStyle } = styles;
+  const { textStyle, viewStyle } = styles;
 
-  return <Text style={textStyle}>Albums!</Text>;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>;
+    </View>
+  );
 };
 
 const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
   textStyle: {
     fontSize: 20
   }
