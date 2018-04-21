5c14b1774613e50e6da23c6dc1e2b4bccac6bce3
Yangchen Shen <shyangch@gmail.com>
2018-04-21 15:03:50 -0700

    Update metro and register the App component

---
+++ index.ios.js
@@ -0,0 +1,13 @@
+// Index.ios.js - place code in here for IOS
+
+// import a library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// create a component, using JSX
+const App = () => (
+    <Text>Some Text</Text>
+);
+
+// render it to the device
+AppRegistry.registerComponent('albums', () => App);
